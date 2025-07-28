f31d28aab361a16b001f92778bd4aa4c00ca0020
karthikpachipala <pachipala.125413@marwadiuniversity.ac.in>
2025-07-28 20:04:29 +0530

    Create karrthikfile.cpp

---
+++ karrthikfile.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+ int main (){
+ int a,b,c,d;
+ cout<<"enter the a and b";
+ cin>>a>>b;
+
+ c=a-b;
+ cout<< "the differnce is: ";
+ cout<<c;
+ d=a/b;
+ cout<<"the quentient is:";
+ cout<<d;
+ }
