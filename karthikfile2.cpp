973b4024d11303588b28bb100cd85f8e5b36bb7e
karthikpachipala <pachipala.125413@marwadiuniversity.ac.in>
2025-07-28 20:16:22 +0530

    Create karthikfile2.cpp

---
+++ karthikfile2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+ int main (){
+ int a,b,c,d;
+ cout<<"enter the a and b";
+ cin>>a>>b;
+
+ c=a+b;
+ cout<< "the addition is: ";
+ cout<<c<<"\n";
+ d=a*b;
+ cout<<"the multiplication is:";
+ cout<<d;
+ }
